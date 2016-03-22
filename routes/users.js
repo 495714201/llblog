64c9ca93288a53b14c521631617d82a75444e3c4
495714201 <495714201@qq.com>
2016-03-22 18:02:27 +0800

    到9课时了，9课时继续

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/reg', function(req, res, next) {
+  res.render('user/reg',{title:'注册'});
 });
+
+router.post('/reg', function(req, res, next) {
+
+});
+
+
+router.get('/login', function(req, res, next) {
+    res.render('user/login',{title:'登录'});
+});
+
+router.post('/login', function(req, res, next) {
+});
+
+router.post('/login', function(req, res, next) {
+});
+
+
 
 module.exports = router;
