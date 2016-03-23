90fbf42849d9c49d46372c5a478c47d28be57af7
495714201 <495714201@qq.com>
2016-03-23 16:17:56 +0800

    dd

---
+++ db/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by LL on 2016/3/23.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    models = require('./models');
+
+var settings = require('../settings');
+mongoose.connect(settings.url);
+mongoose.model('User', new Schema(models.User));
+mongoose.model('Article', new Schema(models.Article));
+
+global.Model = function (type) {
+    return mongoose.model(type);;
+}
---
+++ settings.js
@@ -0,0 +1,10 @@
+/**
+ * Created by LL on 2016/3/23.
+ */
+module.exports = {
+    cookieSecret:'zhufengkey',
+    db:'zhufengblog',
+    host:'123.57.143.189',
+    port:27017,
+    url:"mongodb://123.57.143.189:27017/zhufengblog"
+}
